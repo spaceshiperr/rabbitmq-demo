530cdb9d018dc61ac820c2298e3d55d32037a9f5
Dinara Faizullina <dinara.faizullina@tages.ru>
2023-05-11 16:46:46 +0300

    5-topics tutorial

---
+++ 5-topics/receive_logs_topic.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func returnErr(err error, msg string) {
+	log.Panicf("%s: %s", msg, err)
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		returnErr(err, "Failed to connect to RabbitMQ")
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		returnErr(err, "Failed to open a channel")
+	}
+	defer ch.Close()
+
+	if err := ch.ExchangeDeclare(
+		"logs_topic",
+		"topic",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	); err != nil {
+		returnErr(err, "Failed to declare an exchange")
+	}
+
+	q, err := ch.QueueDeclare(
+		"",
+		false,
+		false,
+		true,
+		false,
+		nil,
+	)
+	if err != nil {
+		returnErr(err, "Failed to declare a queue")
+	}
+
+	if len(os.Args) < 2 {
+		log.Printf("Usage: %s [info] [warning] [error]", os.Args[0])
+		os.Exit(0)
+	}
+
+	for _, s := range os.Args[1:] {
+		log.Printf("Binding queue %s to exchange %s with routing key %s",
+			q.Name, "logs_topic", s)
+		if err = ch.QueueBind(
+			q.Name,       // queue name
+			s,            // routing key
+			"logs_topic", // exchange
+			false,
+			nil,
+		); err != nil {
+			returnErr(err, "Failed to bind a queue")
+		}
+	}
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		returnErr(err, "Failed to register a consumer")
+	}
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf(" [x] %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
